51ebbcb13a57585581b16241e70c62a23c8f0d3a
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2025-12-26 11:51:30 +0530

    Time: 38 ms (99.31%) | Memory: 18.1 MB (77.33%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,19 +1,18 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        maxi = 0
-        profit = 0
-        index = 0
-
-        for i, x in enumerate(customers):
-            if x == 'Y':
-                profit += 1
+        c = customers
+        length = len(c)
+        result = 0
+        resultIdx = -1
+        accum = 0
+        for i in range(length):
+            if c[i] == "Y":
+                accum += 1
             else:
-                profit -= 1
-
-            if profit > maxi:
-                maxi = profit
-                index = i
-
-        if maxi == 0:
-            return 0
-        return index + 1
+                accum -= 1
+            
+            if accum > result:
+                result = accum
+                resultIdx = i
+        
+        return resultIdx + 1
