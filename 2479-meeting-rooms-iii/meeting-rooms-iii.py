24beb04389c0542de78c87827e9fd80825f8792c
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2025-12-27 11:13:25 +0530

    Time: 145 ms (94.99%) | Memory: 51.1 MB (92.37%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,25 +1,32 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        meetings.sort(key = lambda x : x[0])
-        count = [0] * n 
-        freeRoom = list(range(n))
-        heapq.heapify(freeRoom)
-        used = []
+        meetings.sort(key=lambda x: x[0])
+
+        used = [0] * n
+
+        avail = list(range(n))
+        heapify(avail)
+        busy = []
+        heapify(busy)
+
         for start, end in meetings:
-            while used and used[0] and used[0][0] <= start:
-                _, room = heapq.heappop(used)
-                heapq.heappush(freeRoom, room)
-            dur = end - start
-            if freeRoom:
-                room = heapq.heappop(freeRoom)
-                begin = start
+            while busy and busy[0] and busy[0][0] <= start:
+                _,room = heappop(busy)
+                heappush(avail,room)
+            
+            if avail:
+                room = heappop(avail)
+                heappush(busy,(end,room))
+                used[room] +=1
             else:
-                
-                delay, room = heapq.heappop(used)
-                begin = delay
-            count[room] += 1
-            heapq.heappush(used, (begin + dur, room))
-        maxCount = max(count)
+                dur= end-start
+                endTime,room = heappop(busy)
+                heappush(busy,(endTime+dur,room))
+                used[room]+=1
+        maxRoom = 0
+        maxCount = 0
         for i in range(n):
-            if count[i] == maxCount:
-                return i
+            if used[i] > maxCount:
+                maxCount = used[i]
+                maxRoom = i 
+        return maxRoom
