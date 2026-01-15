f36bb06ddb0f1d37d37f001a078c8f34dba63225
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2026-01-15 15:22:30 +0530

    Time: 4 ms (26.15%) | Memory: 19.4 MB (13.85%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.py
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.py
@@ -1,12 +1,25 @@
 class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
-        def maxLen(Bars: List[int]):
-            count,length = 2,2
-            for i in range(1,len(Bars)):
-                if Bars[i] - Bars[i-1] == 1: count += 1
-                else: count = 2
-                length = max(length, count)
-            return length
-        hBars.sort(), vBars.sort()
-        side = min(maxLen(hBars), maxLen(vBars))
-        return side * side
+        h = len(hBars)
+        v = len(vBars)
+        hBars.sort()
+        vBars.sort()
+
+        def find_continuous_chunk(nums) -> list:
+            if not nums:
+                print("warning: empty inputs")
+                return []
+            counts = 1
+            res = 0
+            n = len(nums)
+            for i in range(n-1):
+                if nums[i] == nums[i+1] - 1:
+                    counts += 1
+                    res = max(res, counts)
+                else:
+                    counts = 1
+            return max(res, counts)
+        
+        hcounts = find_continuous_chunk(hBars)
+        vcounts = find_continuous_chunk(vBars)
+        return (min(vcounts, hcounts)+1) ** 2
