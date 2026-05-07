6cfc68fe81118cbd07a33c85092b2e300d73460b
Yagyansh Singh Deshwal <yagyansh.singh.cse.2022@miet.ac.in>
2026-05-07 22:02:08 +0530

    Time: 151 ms (96.55%) | Memory: 38.3 MB (95.69%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.py
+++ 3981-jump-game-ix/jump-game-ix.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
-        
-        suffix = [inf]
-        for i in range(len(nums) - 1, -1, -1):
-            suffix.append(min(suffix[-1], nums[i]))
-        
-        suffix = (suffix[::-1])
+        n = len(nums)
 
-        heap = []
-        ans = [0] * len(nums)
-        mx = 0
-        for i in range(len(nums)):
-            heapq.heappush(heap, (i))
-            mx = max(mx, nums[i])
-            while heap and mx <= suffix[i + 1]:
-                ans[heapq.heappop(heap)] = mx
+        pre_max = [0] * n
+        pre_max[0] = nums[0]
+        cur = nums[0]
+
+        for i in range(1, n):
+            cur = max(nums[i], cur)
+            pre_max[i] = cur
         
-        return (ans)
+
+        suf_min = nums[-1]
+        for i in range(n - 2, -1, -1):
+            if pre_max[i] > suf_min:
+                pre_max[i] = pre_max[i + 1]
+            suf_min = min(suf_min, nums[i])
+        return pre_max
