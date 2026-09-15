5d32f6127b40d5277326f5a0978b464f6e41d583
Yagyansh Singh Deshwal <yagyanshdeshwal@gmail.com>
2026-09-15 11:34:55 +0530

    Time: 1376 ms (97.62%) | Memory: 24.7 MB (79.53%) - LeetSync

--- 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.py
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.py
@@ -1,65 +1,80 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def minMoves(self, classroom: List[str], energy: int) -> int:
-        m, n = len(classroom), len(classroom[0])
-        total_cells = m * n
-        
-        start_pos = -1
-        litter_bit = [0] * total_cells
-        is_reset = [False] * total_cells
-        num_L = 0
-        
+        m = len(classroom)
+        n = len(classroom[0])
+
+        id = [[-1] * n for _ in range(m)]
+
+        k = 0
+        sr = 0
+        sc = 0
+
         for r in range(m):
             for c in range(n):
-                u = r * n + c
-                cell = classroom[r][c]
-                if cell == 'S':
-                    start_pos = u
-                elif cell == 'L':
-                    litter_bit[u] = 1 << num_L
-                    num_L += 1
-                elif cell == 'R':
-                    is_reset[u] = True
-                    
-        target_mask = (1 << num_L) - 1
-        num_masks = 1 << num_L
-        
-        adj = [[] for _ in range(total_cells)]
-        for r in range(m):
-            for c in range(n):
-                if classroom[r][c] == 'X':
-                    continue
-                u = r * n + c
-                for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and classroom[nr][nc] != 'X':
-                        adj[u].append(nr * n + nc)
-                        
-        max_energy = [-1] * (total_cells * num_masks)
-        
-        queue = deque([(start_pos, 0, energy)])
-        max_energy[start_pos * num_masks + 0] = energy
-        
-        moves = 0
-        
+                if classroom[r][c] == 'S':
+                    sr = r
+                    sc = c
+                elif classroom[r][c] == 'L':
+                    id[r][c] = k
+                    k += 1
+
+        if k == 0:
+            return 0
+
+        total_mask = (1 << k) - 1
+
+        best = [
+            [
+                [-1] * (1 << k)
+                for _ in range(n)
+            ]
+            for _ in range(m)
+        ]
+
+        queue = deque()
+
+        best[sr][sc][0] = energy
+        queue.append((sr, sc, 0, energy, 0))
+
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
         while queue:
-            for _ in range(len(queue)):
-                u, mask, e = queue.popleft()
-                
-                if mask == target_mask:
-                    return moves
-                    
-                if e == 0:
+            r, c, mask, e, moves = queue.popleft()
+
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+
+                if nr < 0 or nr >= m or nc < 0 or nc >= n:
                     continue
-                    
-                for v in adj[u]:
-                    nxt_e = energy if is_reset[v] else e - 1
-                    nxt_mask = mask | litter_bit[v]
-                    state_idx = v * num_masks + nxt_mask
-                    
-                    if nxt_e > max_energy[state_idx]:
-                        max_energy[state_idx] = nxt_e
-                        queue.append((v, nxt_mask, nxt_e))
-                        
-            moves += 1
-            
+
+                if classroom[nr][nc] == 'X':
+                    continue
+
+                ne = e - 1
+
+                if ne < 0:
+                    continue
+
+                nmask = mask
+
+                if classroom[nr][nc] == 'R':
+                    ne = energy
+
+                if classroom[nr][nc] == 'L':
+                    nmask |= 1 << id[nr][nc]
+
+                if nmask == total_mask:
+                    return moves + 1
+
+                if ne <= best[nr][nc][nmask]:
+                    continue
+
+                best[nr][nc][nmask] = ne
+
+                queue.append((nr, nc, nmask, ne, moves + 1))
+
         return -1
